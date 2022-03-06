6eda48490e97a54e3a5ab828bb0ab86c8e49e657
Jason S. Robinson <jasonsrobinson@protonmail.com>
2022-03-06 17:24:04 -0500

    Webactions setup

---
+++ src/controller.lisp
@@ -0,0 +1,26 @@
+(defpackage controller
+  (:documentation "EMIS Dynamic control component")
+  (:use #:cl
+	#:net.aserve
+	#:acl-compat.excl
+	#:net.html.generator))
+
+(in-package :controller)
+
+;;; DATA DEFINITIONS 
+
+(defparameter *htdocs* 
+  (namestring (asdf:system-relative-pathname "portfolio" "src/"))
+  "Location of the Static files and templates")
+
+;;; WEBACTION --- Project Description
+
+(webaction-project "portfolio"
+		   :destination *htdocs*
+		   :index "home"
+		   :map '(("home" "home.clp")
+			  ("projects" "projects.clp")
+			  ("contact" "contact.clp")
+			  ("blog" "https://rightmindedtech.blogspot.com")))
+
+
