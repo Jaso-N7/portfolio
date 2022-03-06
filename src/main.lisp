39d420fb6bb144f7de9ce73d6b77d7709394fcab
Jason S. Robinson <jasonsrobinson@protonmail.com>
2022-03-06 17:22:38 -0500

    Project initializations.

--- src/main.lisp
+++ src/main.lisp
@@ -1,5 +1,30 @@
+(in-package :cl-user)
+
+(defun initialize-application (&key port)
+  
+  (net.aserve:start :port port))
+
 (defpackage portfolio
-  (:use :cl))
+  (:documentation "Main entry to Coffee.")
+  (:use #:cl
+	#:net.aserve
+	#:controller)
+  (:export #:start-server
+	   #:stop-server))
+
 (in-package :portfolio)
 
-;; blah blah blah.
+;;; FUNCTIONS --- Project support 
+
+(let ((httpd nil))
+  (defun start-server (&key (port 8080))
+    (setf httpd (cl-user::initialize-application :port port))
+    (format t "~&Portable AllegroServe server STARTED on PORT :~A~%" port)
+    #+unix
+    (format t "~&With PID: ~A~%" (net.aserve::getpid))
+    httpd)
+
+  (defun stop-server (&key (server httpd))
+    (setf httpd (net.aserve:shutdown :server server))
+    (print "Portable AllegroServe server STOPPED.")
+    httpd))
